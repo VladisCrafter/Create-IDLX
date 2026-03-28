5d92d994ee9c100d1e14db185adff369951e2806
VladisCrafter <122053125+VladisCrafter@users.noreply.github.com>
2026-03-28 18:06:30 +0300

    Add CurrentTargetFloorDisplaySource
    I hope somebody will use it after all.

--- src/main/java/com/vladiscrafter/createidlx/registry/CreateIDLXDisplaySources.java
+++ src/main/java/com/vladiscrafter/createidlx/registry/CreateIDLXDisplaySources.java
@@ -6,6 +6,7 @@ import com.simibubi.create.api.registry.CreateBuiltInRegistries;
 import com.vladiscrafter.createidlx.CreateIDLX;
 import com.vladiscrafter.createidlx.content.source.CountdownDisplaySource;
 import com.vladiscrafter.createidlx.content.source.CurrentFloorExtendedDisplaySource;
+import com.vladiscrafter.createidlx.content.source.CurrentTargetFloorDisplaySource;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
@@ -14,12 +15,16 @@ import net.neoforged.neoforge.registries.RegisterEvent;
 public class CreateIDLXDisplaySources {
 
     public static final DisplaySource CURRENT_FLOOR_EXTENDED = new CurrentFloorExtendedDisplaySource();
+    public static final DisplaySource CURRENT_TARGET_FLOOR = new CurrentTargetFloorDisplaySource();
+
     public static final DisplaySource COUNTDOWN = new CountdownDisplaySource();
 
     public static void register(RegisterEvent event) {
         if (!event.getRegistryKey().equals(CreateBuiltInRegistries.DISPLAY_SOURCE.key())) return;
 
         registerByBlock(CURRENT_FLOOR_EXTENDED, "current_floor_extended", AllBlocks.ELEVATOR_CONTACT.get()); // TODO: remake with Record
+        registerByBlock(CURRENT_TARGET_FLOOR, "current_target_floor", AllBlocks.ELEVATOR_CONTACT.get());
+
         registerByBlock(COUNTDOWN, "countdown", AllBlocks.CUCKOO_CLOCK.get());
     }
 
