67a04a729b46a186726ffe33c8fca82cb03b6e37
VladisCrafter <122053125+VladisCrafter@users.noreply.github.com>
2026-04-18 21:54:31 +0300

    Add Clipboard-related config entries
    The final stretch!

--- src/main/java/com/vladiscrafter/createidlx/util/CreateIDLXCustomBlockOutlineRenderer.java
+++ src/main/java/com/vladiscrafter/createidlx/util/CreateIDLXCustomBlockOutlineRenderer.java
@@ -2,6 +2,7 @@ package com.vladiscrafter.createidlx.util;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.vladiscrafter.createidlx.config.CIDLXConfigs;
 import net.minecraft.util.Mth;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -9,6 +10,10 @@ public class CreateIDLXCustomBlockOutlineRenderer {
     public enum Type { DISPLAY_LINK, DISPLAY_LINK_INVALID }
 
     public static void renderShape(VoxelShape s, PoseStack ms, VertexConsumer vb, Type type) {
+        boolean isCustomOutlineForDisplayLinksEnabled = CIDLXConfigs.client.enableCustomOutlineForDisplayLinks.get();
+        boolean isDifferentOutlineForInvalidDisplayLinksEnabled = CIDLXConfigs.client.enableDifferentOutlineForInvalidDisplayLinks.get();
+        float customOutlineAlpha = CIDLXConfigs.client.customOutlineAlpha.getF();
+
         PoseStack.Pose transform = ms.last();
         s.forAllEdges((x1, y1, z1, x2, y2, z2) -> {
             float xDiff = (float) (x2 - x1);
@@ -24,21 +29,21 @@ public class CreateIDLXCustomBlockOutlineRenderer {
             float g = 255f;
             float b = 255f;
 
-            if (type == Type.DISPLAY_LINK) {
+            if (isCustomOutlineForDisplayLinksEnabled && (type == Type.DISPLAY_LINK || (type == Type.DISPLAY_LINK_INVALID && !isDifferentOutlineForInvalidDisplayLinksEnabled))) {
                 r = 131f;
                 g = 239f;
                 b = 145f;
-            } else if (type == Type.DISPLAY_LINK_INVALID) {
+            } else if (isCustomOutlineForDisplayLinksEnabled && type == Type.DISPLAY_LINK_INVALID) {
                 r = 239f;
                 g = 71f;
                 b = 85f;
             }
 
             vb.addVertex(transform.pose(), (float) x1, (float) y1, (float) z1)
-                    .setColor(r / 255f, g / 255f, b / 255f, .8f)
+                    .setColor(r / 255f, g / 255f, b / 255f, (isCustomOutlineForDisplayLinksEnabled ? customOutlineAlpha : .4f))
                     .setNormal(transform.copy(), xDiff, yDiff, zDiff);
             vb.addVertex(transform.pose(), (float) x2, (float) y2, (float) z2)
-                    .setColor(r / 255f, g / 255f, b / 255f, .8f)
+                    .setColor(r / 255f, g / 255f, b / 255f, (isCustomOutlineForDisplayLinksEnabled ? customOutlineAlpha : .4f))
                     .setNormal(transform.copy(), xDiff, yDiff, zDiff);
 
         });
