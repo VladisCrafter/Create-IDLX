6c3f7d62765c4b92bd0483401be2ed95a2068650
VladisCrafter <122053125+VladisCrafter@users.noreply.github.com>
2026-06-04 22:14:24 +0300

    Fix ExpandedEditBox injection cancelling tooltip caching

--- src/main/java/com/vladiscrafter/createidlx/mixin/create/displayLink/source/SingleLineDisplaySourceClientMixin.java
+++ src/main/java/com/vladiscrafter/createidlx/mixin/create/displayLink/source/SingleLineDisplaySourceClientMixin.java
@@ -7,35 +7,39 @@ import com.simibubi.create.foundation.utility.CreateLang;
 import com.vladiscrafter.createidlx.util.gui.CreateIDLXGuiTooltipBuffer;
 import com.vladiscrafter.createidlx.util.widget.ModularGuiLineBuilderExt;
 import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 @Mixin(SingleLineDisplaySource.class)
 public abstract class SingleLineDisplaySourceClientMixin {
 
-    @Inject(method = "addLabelingTextBox", at = @At(value = "HEAD"), cancellable = true)
+    @Unique
+    List<Component> createidlx$labelTooltip = ImmutableList.of(
+            CreateLang.translateDirect("display_source.label")
+                    .withStyle(s -> s.withColor(0x5391E1)),
+            CreateLang.translateDirect("gui.schedule.lmb_edit")
+                    .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC));
+
+    @Inject(method = "addLabelingTextBox", at = @At("HEAD"))
+    private void createidlx$cacheLabelingTextBoxTooltip(ModularGuiLineBuilder builder, CallbackInfo ci) {
+        CreateIDLXGuiTooltipBuffer.registerTooltip("LabelingTextBox", createidlx$labelTooltip);
+    }
+
+    @Inject(method = "addLabelingTextBox", at = @At("HEAD"), cancellable = true)
     private void createidlx$replaceLabelingTextBox(ModularGuiLineBuilder builder, CallbackInfo ci) {
         ((ModularGuiLineBuilderExt) builder).createidlx$addLengthUnrestrainedTextInput(0, 137, (e, t) -> {
             e.setValue("");
-            t.withTooltip(ImmutableList.of(CreateLang.translateDirect("display_source.label")
-                            .withStyle(s -> s.withColor(0x5391E1)),
-                    CreateLang.translateDirect("gui.schedule.lmb_edit")
-                            .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC)));
+            t.withTooltip(createidlx$labelTooltip);
         }, "Label");
         ci.cancel();
-    }
-
-    @Inject(method = "addLabelingTextBox", at = @At("TAIL"))
-    private void createidlx$cacheLabelingTextBoxTooltip(ModularGuiLineBuilder builder, CallbackInfo ci) {
-        CreateIDLXGuiTooltipBuffer.registerTooltip("LabelingTextBox", ImmutableList.of(
-                CreateLang.translateDirect("display_source.label")
-                        .withStyle(s -> s.withColor(0x5391E1)),
-                CreateLang.translateDirect("gui.schedule.lmb_edit")
-                        .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC)));
     }
 }
