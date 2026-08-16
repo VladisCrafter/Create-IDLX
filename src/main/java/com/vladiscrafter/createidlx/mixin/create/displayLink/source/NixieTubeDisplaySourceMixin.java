4c8d5130f8c300efb41cb722c01749b2e49ae04f
VladisCrafter <122053125+VladisCrafter@users.noreply.github.com>
2026-08-16 22:42:11 +0300

    Disallow labeling for NixieTubeDisplaySource for a single edge case
    Now it kinda makes sense why they coded a limit like that. The Attached Label functionality will now be disabled in case of the Target Nixie Tube row being the Source for itself, to prevent the Stack Overflow.

--- src/main/java/com/vladiscrafter/createidlx/mixin/create/displayLink/source/NixieTubeDisplaySourceMixin.java
+++ src/main/java/com/vladiscrafter/createidlx/mixin/create/displayLink/source/NixieTubeDisplaySourceMixin.java
@@ -2,17 +2,43 @@ package com.vladiscrafter.createidlx.mixin.create.displayLink.source;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.api.behaviour.display.DisplayTarget;
 import com.simibubi.create.content.redstone.displayLink.DisplayLinkContext;
 import com.simibubi.create.content.redstone.displayLink.source.NixieTubeDisplaySource;
 import com.simibubi.create.content.redstone.displayLink.target.NixieTubeDisplayTarget;
 import com.vladiscrafter.createidlx.config.CIDLXConfigs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(NixieTubeDisplaySource.class)
 public class NixieTubeDisplaySourceMixin {
+    @SuppressWarnings("DataFlowIssue")
     @ModifyReturnValue(method = "allowsLabeling", at = @At("RETURN"))
     protected boolean allowsLabeling(boolean original, @Local(argsOnly = true) DisplayLinkContext context) {
-        return CIDLXConfigs.server.enhanceNixieTubeDisplaySource.get() || !(context.blockEntity().activeTarget instanceof NixieTubeDisplayTarget);
+        boolean allowLabeling;
+
+        DisplayTarget activeTarget;
+
+        try {
+            LevelAccessor level = Minecraft.getInstance().level;
+
+            BlockPos sourcePos = context.getSourceBlockEntity().getBlockPos();
+            BlockPos targetPos = context.getTargetBlockEntity().getBlockPos();
+
+            activeTarget = DisplayTarget.get(level, context.blockEntity().getTargetPosition());
+
+            AABB fullTarget = activeTarget.getMultiblockBounds(level, targetPos);
+
+            allowLabeling = !(fullTarget.intersects(new AABB(sourcePos)));
+        } catch (NullPointerException e) {
+            activeTarget = context.blockEntity().activeTarget;
+            allowLabeling = false;
+        }
+
+        return CIDLXConfigs.server.enhanceNixieTubeDisplaySource.get() && allowLabeling || !(activeTarget instanceof NixieTubeDisplayTarget);
     }
 }
